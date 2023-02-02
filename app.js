d832c7cd02ad50e1c16b792821c8e07fe003f175
Cate <anncatherinedeguia@gmail.com>
2023-02-02 18:27:17 +0800

    Add a clearGrid function to remove the existing grid

--- app.js
+++ app.js
@@ -1,39 +1,33 @@
-// let container = document.querySelector('#container')
- 
-
-// for(i = 0; i < 257; i ++){
-//     let div = document.createElement('div');
-//     div.setAttribute('id','grid-item')
-//     div.textContent = ''
-//     container.appendChild(div)
-// }
-
 let container = document.querySelector('#container')
-
+let numberOfSquare = 0;
 
 let size = document.querySelector('#size')
+const grid = document.getElementById('container')
 
-function clearInput(){
-      if (numOfSquares.value !="") {
-         numOfSquares.value = "";
-      }
+function newGrid() {
+    numOfSquares = prompt('number of squares per side: ')
+
+    if(numOfSquares > 100) {
+        alert('Number of square per side should be less than 100')
+    }else{
+        for(i = 0; i < numOfSquares * numOfSquares; i ++){
+
+            let div = document.createElement('div');
+            div.className = 'grid-item';
+            div.textContent = ''
+            container.appendChild(div)
+            console.log(numOfSquares)
+        }
+        document.getElementById("container").style.gridTemplateColumns = "repeat(" + numOfSquares + ", 1fr)";
+    }
+}
+
+function clearGrid() {
+    grid.innerHTML = ''
 }
 
 size.addEventListener('click', () => {
-
-   
-
-    numOfSquares = prompt('number of squares per side: ')
-    
-    for(i = 0; i < numOfSquares * numOfSquares; i ++){
-        let div = document.createElement('div');
-        div.className = 'grid-item';
-        div.textContent = ''
-    
-        container.appendChild(div)
-    }
-    document.getElementById("container").style.gridTemplateColumns = "repeat(" + numOfSquares + ", 1fr)";
-
-    clearInput();
+    clearGrid()
+    newGrid()
 })
-
+    
