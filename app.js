5188bc77cc1b593bfcf8af9f24cf45846bbd555f
Cate <anncatherinedeguia@gmail.com>
2023-02-02 14:30:57 +0800

    Add button to change number of squares per side

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
+// let container = document.querySelector('#container')
+ 
+
+// for(i = 0; i < 257; i ++){
+//     let div = document.createElement('div');
+//     div.setAttribute('id','grid-item')
+//     div.textContent = ''
+//     container.appendChild(div)
+// }
+
 let container = document.querySelector('#container')
 
-for(i = 0; i < 256; i ++){
-    let div = document.createElement('div');
-    div.className = 'grid-item';
-    div.textContent = ''
 
-    container.appendChild(div)
+let size = document.querySelector('#size')
+
+function clearInput(){
+      if (numOfSquares.value !="") {
+         numOfSquares.value = "";
+      }
 }
 
+size.addEventListener('click', () => {
 
+   
+
+    numOfSquares = prompt('number of squares per side: ')
     
+    for(i = 0; i < numOfSquares * numOfSquares; i ++){
+        let div = document.createElement('div');
+        div.className = 'grid-item';
+        div.textContent = ''
+    
+        container.appendChild(div)
+    }
+    document.getElementById("container").style.gridTemplateColumns = "repeat(" + numOfSquares + ", 1fr)";
+
+    clearInput();
+})
+
